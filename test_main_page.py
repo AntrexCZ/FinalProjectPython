c02f7c9e596ae1f1c94e8aa75790e40806ca819c
Andrii Iesmienieiev <antrex@email.cz>
2022-05-05 08:30:49 +0200

    main and base pages,exception handling

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,14 @@
+from .pages.main_page import MainPage
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
+    page = MainPage(browser, link) # Initialize PAge Object, send url to constructor
+    page.open() # open the page
+    page.go_to_login_page() # execute method of the page, go to login page
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
