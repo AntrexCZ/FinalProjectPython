f565a863d10b9444ae57605c2a4e4e1ecee228ba
Andrii Iesmienieiev <antrex@email.cz>
2022-05-10 10:19:43 +0200

    new updates :)

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,19 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "login_link")
+#class MainPageLocators():
 
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    BOOK_BASKET_PRICE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    BOOK_BASKET_NAME = (By.CSS_SELECTOR, "#messages>div:nth-child(1) strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.ID, "login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
---
+++ pages/product_page.py
@@ -0,0 +1,45 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+
+
+class ProductPage(BasePage):
+
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET)
+
+    def should_be_winter_promo(self):
+        # Check if the link is New Year promo
+        assert "promo=newYear" in self.url, "This is not New Year's Promo"
+
+    def add_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_button.click()
+
+    def should_be_success_message(self):
+        assert "has been added to your basket." in self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE)
+
+    def should_be_book_price_same_as_basket_book_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_book_price = self.browser.find_element(*ProductPageLocators.BOOK_BASKET_PRICE).text
+        assert book_price == basket_book_price, "The price of the books in not the same"
+
+    def should_be_book_name_same_as_basket_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        print("Book name : ...", book_name)
+        book_basket_name = self.browser.find_element(*ProductPageLocators.BOOK_BASKET_NAME).text
+        print("Basket : ... ", book_basket_name)
+        assert book_name == book_basket_name, "The name of the books is different"
+
+    def should_be_success_message(self):
+        success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+        print(success_message)
+        assert "has been added to your basket." in success_message, "Success message is not present"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_present_element(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
